b380cf9fd04dab8a60cb596cae365cb62dfe370f
Aksel <acceleratus@hotmail.com>
2016-02-18 08:53:51 +0100

    Update Colorsens.py
    hope this works

--- Colorsens.py
+++ Colorsens.py
@@ -1,5 +1,4 @@
-from ev3dev.auto import OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, INPUT_3, \
-    INPUT_4, Motor, ColorSensor, InfraredSensor, Button
+from ev3dev.auto import *
 import time
 
 # IO Aliases
@@ -10,24 +9,43 @@ MtRight = Motor(OUTPUT_C)
 MtGrab = Motor(OUTPUT_D)
 
 InInfra = InfraredSensor(INPUT_3)
-InColor = ColorSensor(INPUT_4)
-#InEnter = Button.enter()
+InColor = ColorSensor(INPUT_1)
+InTouch = TouchSensor(INPUT_4)
 
+# Musical tones (In Hertz)
+
+C = 261.63
+Cs = 277.18
+D = 293.66
+Ds = 311.13
+E = 329.63
+F = 349.23
+Fs = 369.99
+G = 392
+Gs = 415.3
+A = 440
+As = 466.16
+B = 493.88
+C2 = 523.25
+
+MajArp = (C,100),(E,100),(G,100),(C2,100)
+MinArp = (C,100),(Ds,100),(G,100),(C2,100)
 # Main Sequence
 """
-This sequence should let the robot drive forward until the Color Sensor detects
-a dark line, at which point it will turn away from it. """
+This tests various features of the robot. """
 
-x = 1
+Sound.tone(MajArp)
+
 while True:
-    MtLeft.run_forever(duty_cycle_sp=75)
-    MtRight.run_forever(duty_cycle_sp=75)
+    MtRight.run_forever(duty_cycle_sp=85)
+    MtLeft.run_forever(duty_cycle_sp=100)
+    if Button.enter == 1:
+        break
+    else:
+        if ColorSensor.units < 30:
+            MtLeft.stop()
+            time.sleep(0.3)
 
-if InColor < 30:
-    MtRight.stop()
-    time.sleep(0.1)
-
-#if InEnter == True:
-#    quit()
-
-x += 1
+MtRight.stop()
+MtLeft.stop()
+Sound.speak("That was fun. We should do that again!")
